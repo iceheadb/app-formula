8d2f2c8d401f183085cf794aa83a4ac578146dab
iceheadb <victorlopeszb@gmail.com>
2023-05-14 16:54:56 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from './components/Loading';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Loading />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#16357a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fefefe',
+  }
 });
